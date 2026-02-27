ef401c58ee53ed0d4efebc06bbbfd1db12e315e8
AndreiKlinchev <klinchevstudy@gmail.com>
2026-02-27 08:27:53 +0200

    initial commit for lab 3

--- phpLab2/index.php
+++ phpLab2/index.php
@@ -1,15 +1,15 @@
 <?php
+
 //вывод 1
 echo "Привет, мир!", "ДА ДА МИР, ПРИВЕТ!";
 //вывод 2
-echo "</br>Hello, World with echo!";
-print "</br>Hello, World with print!";
+echo "<br />Hello, World with echo!";
+print "<br />Hello, World with print!";
 
 //вывод 3
 
 $days = 288;
 $message = "Всё, возвращайся за работу";
  
-echo "</br>До супур события".$days;
-echo "</br>На пэйджер пришло сообщение: {$message}";
-?>
+echo "<br />До супур события".$days;
+echo "<br />На пэйджер пришло сообщение: {$message}";
